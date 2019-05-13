0cfc4dbac09a643a8d7a525433da952f5f8f0238
Ming Duan <11203duan@gmail.com>
2019-05-13 16:06:32 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,31 @@
+class Show < ActiveRecord::Base
+
+  def Show::highest_rating
+    Show.maximum(:rating)
+  end
+
+  def Show::most_popular_show
+    Show.maximum(:day)
+  end
+
+  def Show::lowest_rating
+    Show.
+  end
+
+  def Show::least_popular_show
+    Show.
+  end
+
+  def Show::ratings_sum
+    Show.
+  end
+
+  def Show::popular_shows
+
+  end
+
+  def Show::shows_by_alphabetical_order
+    Show.
+  end
+
+end
